c7c88f831d6db54bb6442ea5ee83f4e61255b209
Elham N Ghassemi <elhamng@gmail.com>
2021-05-21 16:56:11 +0200

    example for numpy

---
+++ pyprogs/examples_Num_py.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  5 11:06:38 2021
+
+@author: elham
+"""
+
+import numpy as np
+from numpy import random
+
+l = [1,2,3]
+
+lenght = len(l)
+
+print('hello')
+print(type(l))
+
+x = random.rand(5)
+print(x)
+
+z = random.randint(100,size=5,dtype=int)
+print(z)
