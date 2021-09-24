068c50ab60348a89fb72cfdb73f6a5a613941b4e
Elham Nour Ghassemi <elhamng@gmail.com>
2021-09-24 06:12:50 +0200

    add bookstore

---
+++ pyprogs/backend.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 23 16:46:55 2021
+
+@author: elham
+"""
+
+import sqlite3
+
+def connect():
+    conn = sqlite3.connect('books.db')
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+    conn.commit()
+    conn.close()
+    
+    
+
+def insert(title,author,year,isbn):
+    conn = sqlite3.connect('books.db')
+    cur = conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)", (title,author,year,isbn))
+    conn.commit()
+    conn.close()
+    
+def view():
+    conn =sqlite3.connect('books.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+    
+
+def search(title="",author ="",year="",isbn=""):
+    conn =  sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+    
+
+def delete(id):
+    conn =sqlite3.connect('books.db')
+    cur = conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=?",(id,))
+    conn.commit()
+    conn.close()
+
+def update(id,title,author,year,isbn):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?",(title,author,year,isbn,id))
+    conn.commit()
+    conn.close()
+    
+
+connect()
+#insert("The moon", "John Tablet",1918,913123132)
+#delete(20)
+#update(3,'The sun','John Smith',1999,99999)
+print(view())
+
+
+
+
+
